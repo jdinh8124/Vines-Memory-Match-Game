dcf7c0f986446d9a7a48c161d9b79f0026843bf0
James Dinh <mrjames8124@gmail.com>
2019-11-12 19:47:20 -0800

    figured out the matching of cards

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 $(document).ready(initalizeApp)
 
+
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
+
 function initalizeApp(){
   $(".card").on("click", handleCardClick);
-
 }
 
 
 function handleCardClick(event){
-  $(event.currentTarget).find('.back').toggleClass("hidden");
+  $(event.currentTarget).find('.back').addClass("hidden");
+  if (firstCardClicked === null) {
+    firstCardClicked = $(event.currentTarget);
+    return;
+  } else {
+    secondCardClicked = $(event.currentTarget);
+  }
+
+  if (firstCardClicked.find('.front').css('background-image') === secondCardClicked.find('.front').css('background-image')) {
+    matches += 1;
+    firstCardClicked = null;
+    secondCardClicked = null;
+  } else {
+    setTimeout(function () {
+
+      firstCardClicked.find('.back').removeClass("hidden");
+      secondCardClicked.find('.back').removeClass("hidden");
+      firstCardClicked = null;
+      secondCardClicked = null;
+    }, 1500);
+
+  }
+
 }
